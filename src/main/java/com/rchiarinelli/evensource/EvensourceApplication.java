a1304479ccce292c76ecf6f381c205d4534a4909
Rafael <rchiarinelli@bancomare.com.br>
2019-12-30 21:40:15 +0000

    Initial commit

---
+++ src/main/java/com/rchiarinelli/evensource/EvensourceApplication.java
@@ -0,0 +1,13 @@
+package com.rchiarinelli.evensource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvensourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvensourceApplication.class, args);
+	}
+
+}
