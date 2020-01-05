59437caf16086770dfedf6e1227e1c0431782de3
Rafael <rchiarinelli@bancomare.com.br>
2020-01-05 12:51:47 +0000

    Changes to deploy in dev

--- src/main/java/com/rchiarinelli/eventsource/EventSourceApplication.java
+++ src/main/java/com/rchiarinelli/eventsource/EventSourceApplication.java
@@ -2,18 +2,20 @@ package com.rchiarinelli.eventsource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/info")
 public class EventSourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventSourceApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 	  return "Hello Docker World";
 	}
--- src/test/java/com/rchiarinelli/eventsource/EventSourceApplicationTests.java
+++ src/test/java/com/rchiarinelli/eventsource/EventSourceApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventSourceApplicationTests {
+public class EventSourceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
